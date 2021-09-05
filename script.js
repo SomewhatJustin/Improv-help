6997057503ee0c287ac98456381c540d33b732f2
Justin Fowler <justin.fowler@hey.com>
2021-09-05 18:14:03 -0500

    ignore that

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function checkHttps(req, res, next) {
-  // protocol check, if http, redirect to https
-
-  if (req.get('X-Forwarded-Proto').indexOf("https") != -1) {
-    return next()
-  } else {
-    res.redirect('https://' + req.hostname + req.url);
-  }
-}
-
-app.all('*', checkHttps);
-
 const generateScene = function () {
 
   // LOCATIONS
